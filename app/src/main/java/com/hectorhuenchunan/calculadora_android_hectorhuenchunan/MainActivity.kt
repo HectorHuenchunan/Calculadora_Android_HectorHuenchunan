b66cbf11b4d62f61878ca4eb0f61cd8f204aba46
HectorHuenchunan <hector.huenchunan@correoaiep.com>
2023-09-09 23:24:53 -0300

    final

--- app/src/main/java/com/hectorhuenchunan/calculadora_android_hectorhuenchunan/MainActivity.kt
+++ app/src/main/java/com/hectorhuenchunan/calculadora_android_hectorhuenchunan/MainActivity.kt
@@ -2,7 +2,6 @@ package com.hectorhuenchunan.calculadora_android_hectorhuenchunan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,68 +11,105 @@ class MainActivity : AppCompatActivity() {
     lateinit var num1: TextView
     lateinit var num2: TextView
     lateinit var result: TextView
+    lateinit var Operacion: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnIgual: Button = findViewById(R.id.btnIgual)
-        val btnBorrar: Button = findViewById(R.id.btnBorrar)
-
         num1 = findViewById(R.id.Textview_1)
         num2 = findViewById(R.id.Textview_2)
         result = findViewById(R.id.Textview_Result)
+        Operacion = findViewById(R.id.Textview_Operacion)
 
     }
 
 
 
-    fun SelectionNumber(view: View){
-        var aux_num2 = num2.text.toString()
+    fun SeleccionNumero(view: View){
+        var num1_aux: String = num1.text.toString()
 
         when(view.id){
-            R.id.btn0 -> num2.setText(aux_num2+"0")
-            R.id.btn1 -> num2.setText(aux_num2+"1")
-            R.id.btn2 -> num2.setText(aux_num2+"2")
-            R.id.btn3 -> num2.setText(aux_num2+"3")
-            R.id.btn4 -> num2.setText(aux_num2+"4")
-            R.id.btn5 -> num2.setText(aux_num2+"5")
-            R.id.btn6 -> num2.setText(aux_num2+"6")
-            R.id.btn7 -> num2.setText(aux_num2+"7")
-            R.id.btn8 -> num2.setText(aux_num2+"8")
-            R.id.btn9 -> num2.setText(aux_num2+"9")
-            R.id.btnPunto -> num2.setText(aux_num2+".")
+            R.id.btn0 -> num1.setText(num1_aux + "0")
+            R.id.btn1 -> num1.setText(num1_aux + "1")
+            R.id.btn2 -> num1.setText(num1_aux + "2")
+            R.id.btn3 -> num1.setText(num1_aux + "3")
+            R.id.btn4 -> num1.setText(num1_aux + "4")
+            R.id.btn5 -> num1.setText(num1_aux + "5")
+            R.id.btn6 -> num1.setText(num1_aux + "6")
+            R.id.btn7 -> num1.setText(num1_aux + "7")
+            R.id.btn8 -> num1.setText(num1_aux + "8")
+            R.id.btn9 -> num1.setText(num1_aux + "9")
         }
     }
 
     fun SeleccionOperacion(view: View){
-        var aux_num2 = num2.text.toString()
-        numero1 = num2.toString().toDouble()
-        num2.setText("")
+
+        var num1_aux : String = num1.text.toString()
+
+        num1.setText("")
+
         when(view.id){
-            R.id.btnSumar -> {
-                num1.setText(aux_num2+"+")
+            R.id.btnSumar ->{
+                num2.setText(num1_aux)
+                Operacion.setText("+")
+                result.setText("")
                 oper = 1
             }
             R.id.btnRestar ->{
-                num1.setText(aux_num2+"-")
+                num2.setText(num1_aux)
+                Operacion.setText("-")
+                result.setText("")
                 oper = 2
             }
             R.id.btnMult ->{
-                num1.setText(aux_num2+"*")
+               num2.setText(num1_aux)
+                Operacion.setText("*")
+                result.setText("")
                 oper = 3
             }
-            R.id.btnDividir -> {
-                num1.setText(aux_num2+"/")
+            R.id.btnMult ->{
+               num2.setText(num1_aux)
+                Operacion.setText("/")
+                result.setText("")
                 oper = 4
             }
+
         }
 
-        fun nuevafuncion (view: View){
 
+    }
+
+    fun Resultado (view: View){
+        var num1_aux = num1.text.toString().toDouble()
+        var num2_aux = num2.text.toString().toDouble()
+        var res: Double = 0.0
+        when(oper){
+            1-> res =  num2_aux + num1_aux
+            2-> res = num2_aux - num1_aux
+            3-> res = num2_aux * num1_aux
+            4-> res = num2_aux / num1_aux
+        }
+        result.setText(res.toString())
+        num1.setText(res.toString())
+        num2.setText("")
+        Operacion.setText("")
+
+    }
+
+        fun Borrar (view: View){
+            var txt1: TextView = findViewById(R.id.Textview_1)
+            var txt2: TextView = findViewById(R.id.Textview_2)
+            var txt3: TextView = findViewById(R.id.Textview_Result)
+            var txt4: TextView = findViewById(R.id.Textview_Operacion)
+
+            txt1.setText("")
+            txt2.setText("")
+            txt3.setText("")
+            txt4.setText("")
         }
     }
 
 
-}
+
